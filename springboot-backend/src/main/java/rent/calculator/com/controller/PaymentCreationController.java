661c51fb91415e9a78563a1beb9fda6fcb24948d
RomanDrohobytskyi <romabikebmx@gmail.com>
2023-01-20 19:30:05 +0100

    Controller refactoring

---
+++ springboot-backend/src/main/java/rent/calculator/com/controller/PaymentCreationController.java
@@ -0,0 +1,19 @@
+package rent.calculator.com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import rent.calculator.com.model.dto.PaymentDTO;
+import rent.calculator.com.service.PaymentCreationService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/rent/calculator/")
+@RequiredArgsConstructor
+public class PaymentCreationController {
+    private final PaymentCreationService paymentCreationService;
+
+    @PostMapping("/payments/add")
+    public PaymentDTO add(@RequestBody PaymentDTO paymentDTO) {
+        return paymentCreationService.save(paymentDTO);
+    }
+}
